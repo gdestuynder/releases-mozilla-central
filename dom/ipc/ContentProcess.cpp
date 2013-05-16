edd4c7d638639a6200703560f885f5c249aee2fb
Guillaume Destuynder <kang@mozilla.com>
2013-05-16 15:16:16 -0700

    Basic KSM support based on Cyanogenmod's Zygote

--- dom/ipc/ContentProcess.cpp
+++ dom/ipc/ContentProcess.cpp
@@ -8,19 +8,56 @@
 
 #include "ContentProcess.h"
 
+#include <sys/mman.h>
+#include <stdio.h>
+#define MADV_MERGEABLE 12
+
 using mozilla::ipc::IOThreadChild;
 
 namespace mozilla {
 namespace dom {
-
 bool
 ContentProcess::Init()
 {
+    FILE *fp;
+    char section[100];
+    char perms[5];
+    unsigned long start, end, misc;
+    int ch, offset;
+
     mContent.Init(IOThreadChild::message_loop(),
                          ParentHandle(),
                          IOThreadChild::channel());
     mXREEmbed.Start();
     mContent.InitXPCOM();
+
+    fp = fopen("/proc/self/maps", "r");
+    if (fp != NULL) {
+        while (fscanf(fp, "%lx-%lx %4s %lx %lx:%lx %ld",
+            &start, &end, perms, &misc, &misc, &misc, &misc) == 7)
+        {
+             section[0] = 0;
+             offset = 0;
+             while(1)
+             {
+                 ch = fgetc(fp);
+                 if (ch == '\n' || ch == EOF)
+                     break;
+                 if ((offset == 0) && (ch == ' '))
+                     continue;
+                 if ((offset + 1) < 100) {
+                     section[offset]=ch;
+                     section[offset+1]=0;
+                     offset++;
+                 }
+             }
+            if (( section[0] == 0) || (strcmp(section,"[stack]") == 0) || (strcmp(section,"[heap]") == 0))
+                madvise((void*) start, (size_t) end-start, MADV_MERGEABLE);
+            if (ch == EOF)
+                break;
+        }
+        fclose(fp);
+    }
     
     return true;
 }
